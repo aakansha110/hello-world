2e25fb3bf6356496c7dc9d82102bee0b64a1d9b9
aakansha patel <paakansha2505@gmail.com>
2025-08-22 21:00:11 +0530

    add my first code

---
+++ prog1.py
@@ -0,0 +1,12 @@
+# Practical 4: Addition two numbers using command line arguments.
+
+import sys
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+
+c=a+b
+
+print("a=",a)
+print("b=",b)
+print("sum=",c)
+
